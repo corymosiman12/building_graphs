17654c3b203d72ec20ad9bd817061e97e0de07d2
corymosiman12 <cory.mosiman@nrel.gov>
2019-11-01 17:57:51 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # building-graphs
 Building of Haystack / Brick Graphs
+
+# Introduction
+The purpose of this repo is to create both Brick / Haystack graphs.  The intended workflow is as follows:
+
+1. Use the JSON files in the haystack/ directory copied from [brick-examples](https://github.com/BrickSchema/brick-examples) to create BRICK / Haystack 4.0 graphs
+    - resources/def.ttl includes the Haystack 3.9.7 ttl file
+    - resources/Brick.ttl includes the Brick 1.0.3 ttl file
+    - resources/BrickFrame.ttl includes the Brick Frame 1.0.3 ttl file
+2. Serialize as a ttl file
+3. Utilize a graph database technology (Neo4j) to import the model.
---
+++ scripts/update_ghausi.py
@@ -0,0 +1,33 @@
+import os
+from utils.utils import *
+
+# Define the example file to use for the analysis.
+# Clone the brick-examples repo to the same directory level
+# as this repo.
+ex_dir = os.path.join(os.getcwd(), '../brick-examples/haystack')
+ex_file = os.path.join(ex_dir, "ghausi.json")
+to_write = os.path.join(ex_dir, "ghausi_v2.json")
+
+# Import the json file as a dictionary.
+bldg = import_haystack_json(ex_file)
+
+# Find all equipment entities
+equips = find_equips(bldg)
+
+# Find all fcus based on the navName.
+# The fact that the navName contains FCU was found through
+# exploratory analysis
+fcus, non_fcu_equips = find_str_in_navName(equips, 'fcu')
+
+# Find all point entities
+sensors, not_sensors = find_sensors(bldg)
+cmds, not_cmds = find_cmds(not_sensors)
+sps, not_sps = find_sps(not_cmds)
+
+def add_marker(entities, marker_str):
+    for e in entities:
+        e[marker_str] = "M"
+    return entities
+
+sens = add_marker(sensors, 'point')
+print(sens[0].keys())
